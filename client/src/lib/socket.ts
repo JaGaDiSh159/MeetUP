7bef75b21b5018621f22eac9bef9ed8d31a1c173
Jagadish <jagadishdash2004@gmail.com>
2026-01-01 21:59:51 +0530

    fix socket.io auth cookies and websocket transport

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 import { BACKEND_URL } from "../config";
 
-const socket = io(BACKEND_URL);
+const socket = io(BACKEND_URL, {
+  withCredentials: true,
+  transports: ["websocket"], // avoids polling 404s on Render
+});
 
 export default socket;
