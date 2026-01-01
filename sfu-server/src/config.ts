102799d13a791d775f4fab2ccb45c71f918de1aa
Jagadish <jagadishdash2004@gmail.com>
2026-01-02 00:18:50 +0530

    config.ts changed

--- sfu-server/src/config.ts
+++ sfu-server/src/config.ts
@@ -17,21 +17,23 @@ export const mediaCodecs: mediasoupTypes.RtpCodecCapability[] = [
     },
 ]
 
-
 export const webRtcTransport_options: mediasoupTypes.WebRtcTransportOptions = {
-    listenIps:
-        process.env.NODE_ENV === 'production'
-            ? [
-                {
-                    ip: process.env.WEBRTC_LISTEN_IP || '127.0.0.1',
-                    announcedIp: process.env.WEBRTC_ANNOUNCED_IP || '127.0.0.1',
-                },
-            ]
-            : [{ ip: '127.0.0.1' }],
+  listenIps:
+    process.env.NODE_ENV === 'production'
+      ? [
+          {
+            ip: '0.0.0.0',
+            announcedIp: process.env.WEBRTC_ANNOUNCED_IP!,
+          },
+        ]
+      : [
+          {
+            ip: '127.0.0.1',
+          },
+        ],
 
-    enableUdp: true,
-    enableTcp: true,
-    preferUdp: true,
-
+  enableUdp: true,
+  enableTcp: true,
+  preferUdp: true,
 };
 
