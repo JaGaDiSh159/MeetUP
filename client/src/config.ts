557a07be04936b770cd719ea07c502136cc728df
Jagadish <jagadishdash2004@gmail.com>
2026-01-09 00:32:22 +0530

    Remove hardcoded BACKEND_URL and use environment variable for configuration

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,6 @@
-export const BACKEND_URL="https://meetup-3f10.onrender.com"
+// export const BACKEND_URL="https://meetup-3f10.onrender.com"
+
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL!;
+
 export const clientId="4068692044-usiuhg8kf1mq4htulpgj0s2ag9tafijn.apps.googleusercontent.com"
 
